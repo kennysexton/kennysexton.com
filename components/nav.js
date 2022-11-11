582256d6bc0f346e830c39a119a2fb3bbc1cdaf7
kennysexton <kennysexton4@gmail.com>
2022-11-11 07:39:36 -0700

    adding more to nav component

--- components/nav.js
+++ components/nav.js
@@ -1,19 +1,43 @@
 import Link from 'next/link'
+import { useState } from 'react'
 import styles from '../styles/Nav.module.css'
 
-export default function Nav() {
-  return (
+const defaultName = 'Home'
 
-    <nav className="sticky top-0 h-24 bg-slate-500">
+const navDirectory = [
+  { href: '/', name: defaultName },
+  { href: '/aerial', name: 'Aerial' },
+  { href: '/projects', name: 'Projects' },
+  { href: '/books', name: 'Books' },
+]
+
+export default function Nav() {
+  const [activeNavItem, setActiveNavItem] = useState(defaultName)
+
+  return (
+    <nav className="sticky top-0 h-24">
       <div className="container h-full flex justify-between items-center">
         <div>
-          <Link href="/">Kenny Sexton</Link>
+          <Link
+            onClick={() => setActiveNavItem(defaultName)}
+            className="text-lg"
+            href="/"
+          >
+            Kenny Sexton
+          </Link>
         </div>
-        <div className="flex">
-          <Link href="/">Home</Link>
-          <Link href="/aerial">Aerial</Link>
-          <Link href="/projects">Projects</Link>
-          <Link href="/books">Books</Link>
+        <div className="flex lg:gap-8 sm:gap-2 text-sm text-neutral-500">
+          {navDirectory.map((item) => (
+            <div className="hover:underline hover:underline-offset-8 hover:text-black">
+              <Link
+                onClick={() => setActiveNavItem(item.name)}
+                href={item.href}
+                className={(activeNavItem === item.name) && 'underline underline-offset-8 text-black'}
+              >
+                {item.name}
+              </Link>
+            </div>
+          ))}
         </div>
       </div>
     </nav>
